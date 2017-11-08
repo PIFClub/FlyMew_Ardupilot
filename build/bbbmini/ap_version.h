b785e2b99bde5020a2c84136536c3e3a875ed78e
honghiep <nghiemhonghiep@gmail.com>
2017-11-08 16:05:56 +0700

    Remove built files

--- build/bbbmini/ap_version.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#define GIT_VERSION "466dae2a"
--- build/c4che/_cache.py
+++
@@ -1,7 +0,0 @@
-AUTOCONFIG = True
-BINDIR = '/usr/local/bin'
-BOARD = 'bbbmini'
-DEBUG = False
-LIBDIR = '/usr/local/lib'
-PREFIX = '/usr/local'
-VARIANT = 'bbbmini'
